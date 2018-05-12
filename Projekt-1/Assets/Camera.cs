201662da5d9dc1cc208ba4f47c62ac2ea539bad7
Adam Gramblička <adamgramblicka@hotmail.com>
2018-05-12 16:10:09 +0200

    Movement

---
+++ Projekt-1/Assets/Camera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+    public float cameraSpeed = 10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UnityEngine.Camera.main.transform.position = Vector3.Lerp(UnityEngine.Camera.main.transform.position, transform.position + Vector3.up*2 + Vector3.back*10, Time.deltaTime * cameraSpeed); 
+	}
+}
--- Projekt-1/Assets/Movment.cs
+++ Projekt-1/Assets/Movment.cs
@@ -4,13 +4,52 @@ using UnityEngine;
 
 public class Movment : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    Rigidbody rigidbody;
+    bool grounded = true;
+    public float speed = 25f;
+    public float jumpForce = 100f;
+
+
+    // Use this for initialization
+    void Start() {
+        transform.position = new Vector3(0, 0, 0);
+        rigidbody = GetComponent<Rigidbody>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey(KeyCode.A)) {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+            Debug.Log("A");
+        }
+
+        if (Input.GetKey(KeyCode.S)) {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+            Debug.Log("S");
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+            Debug.Log("D");
+        }
+
+        if (Input.GetKey(KeyCode.W)) {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            Debug.Log("W");
+        }
+
+        if (Input.GetKey(KeyCode.Space)) {
+            if (grounded) {
+                rigidbody.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+                grounded = false;
+                Debug.Log("Jump");
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        grounded = true;
+    }
 }
