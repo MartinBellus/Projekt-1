fc9126eba430029ca26341f3c6115d4907b3dff4
Adam Gramblička <adamgramblicka@hotmail.com>
2018-05-13 17:28:01 +0200

    MovingObject + PickableObject

---
+++ Projekt-1/Assets/MovingObject/MovingObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour {
+
+    public float length = 8f;
+    private Vector3 pos;
+
+	// Use this for initialization
+	void Start () {
+        transform.GetChild(0).transform.position = transform.position - new Vector3(length / 2, 0, 0);
+        pos = transform.GetChild(0).transform.position;  
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.GetChild(0).transform.position = new Vector3(Mathf.PingPong(Time.time, length) + pos.x, transform.position.y, transform.position.z);
+        Debug.DrawLine(pos, new Vector3(pos.x + length, pos.y, pos.z), Color.green);
+    }
+}
+
---
+++ Projekt-1/Assets/Pickable/Pickable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickable : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Pickup(other);
+        }
+    }
+
+    void Pickup(Collider col)
+    {
+        //OPTIONAL Particle effect - Instantiate(PARTICLE,transform.position,Quaternion.identity);
+
+        PlayerStats playerStats = col.GetComponent<PlayerStats>();
+
+        playerStats.coins++;
+
+        Destroy(gameObject);
+    }
+}
